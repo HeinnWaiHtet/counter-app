f844c026ae9917eaee769e025e2322c1c1e0a514
Hein Wai Htet <cuheinwaihtet@gmail.com>
2021-10-17 16:38:24 +0630

    add attributes style

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,10 +5,15 @@ class Counter extends React.Component {
         count : 0,
     };
 
+    styles = {
+        fontSize : 20,
+        fontWeight : "bold",
+    };
+
     render() { 
         return (
         <div>
-            <span className="badge badge-primary m-2">{this.formatCount()}</span>
+            <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment</button>
         </div>);
     }
